f47904a272b5017dc69b87e0d1e54cf24a4ccf75
ale94 <alejandrorua111@gmail.com>
2025-08-13 22:41:52 -0300

    feat: search gifs method services

--- src/app/gifs/pages/search-page/search-page.ts
+++ src/app/gifs/pages/search-page/search-page.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { GifList } from '../../components/gif-list/gif-list';
+import { GifsService } from '../../services/gifs-service';
+import { Gif } from '../../interfaces/gif.interface';
 
 @Component({
   selector: 'app-search-page',
-  imports: [],
-  templateUrl: './search-page.html'
+  imports: [GifList],
+  templateUrl: './search-page.html',
 })
 export default class SearchPage {
 
+  gifsService = inject(GifsService);
+  gifs = signal<Gif[]>([]);
+
+  onSearch(query: string) {
+    this.gifsService.searchGifs(query).subscribe((resp) => {
+      this.gifs.set(resp);
+    });
+  }
 }
--- src/app/gifs/services/gifs-service.ts
+++ src/app/gifs/services/gifs-service.ts
@@ -4,6 +4,7 @@ import { environment } from 'environments/environment';
 import type { GiphyResponse } from '../interfaces/giphy.interfaces';
 import { Gif } from '../interfaces/gif.interface';
 import { GifMapper } from '../mapper/gif.mapper';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -30,7 +31,27 @@ export class GifsService {
         const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
         this.trendingGifs.set(gifs);
         this.trendingGifsLoading.set(false);
-        console.log({ gifs });
+        // console.log({ gifs });
       });
+  }
+
+  searchGifs(query: string) {
+    return this.htpp
+      .get<GiphyResponse>(`${environment.giphyUrl}/gifs/search`, {
+        params: {
+          api_key: environment.giphyApiKey,
+          limit: 20,
+          q: query,
+        },
+      })
+      .pipe(
+        map(({ data }) => data),
+        map((items) => GifMapper.mapGiphyItemsToGifArray(items))
+      );
+
+    // .subscribe((resp) => {
+    //   const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
+    //   console.log({ search: gifs });
+    // });
   }
 }
